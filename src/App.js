44aecfc480d21ce4b5d6f9038fe99c942c84e115
“Igormirowski” <igorpmirowski@gmail.com>
2022-06-16 13:00:16 +0100

    add cards

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+
+
+function Card () {
+  return (
+    <>
+    <div> Hello name</div>
+    <p> Your cohort was: cohort_name</p>
+    </>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Card />
+        <Card />
+        <Card />
       </header>
     </div>
   );
 }
 
 export default App;
+
